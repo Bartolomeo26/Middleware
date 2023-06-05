e57b467fb05fd6c33d739fcc9a846de9d681b5cd
Bartosz Spiżarny <83046@student.pb.edu.pl>
2023-06-05 21:47:12 +0200

    Redirecting working properly

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -1,4 +1,4 @@
-
+using Middleware.Utlis;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,7 +13,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseMiddleware<RedirectToPage>();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
---
+++ Middleware/Utlis/RedirectToPage.cs
@@ -0,0 +1,40 @@
+﻿using DeviceDetectorNET;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Threading.Tasks;
+
+namespace Middleware.Utlis
+{
+    public class RedirectToPage
+    {
+        private readonly RequestDelegate _next;
+
+        public RedirectToPage(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Redirection(HttpContext context)
+        {
+            string userAgent = context.Request.Headers["User-Agent"].ToString();
+            var Parse_userAgent = new DeviceDetector(userAgent);
+            Parse_userAgent.Parse();
+
+            string browserName = Parse_userAgent.GetClient().Match.Name; //przypisanie nazwy użytej przeglądarki do zmiennej
+            if (WhichBrowser(browserName))
+            {
+               
+                    context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/"); //przekierowanie na Firefoxa
+                    return Task.CompletedTask;
+               
+            }
+
+            return _next(context);
+        }
+
+        private bool WhichBrowser(string browserName) //funkcja sprawdzająca, czy weszliśmy na którąś z wymienionych przeglądarek
+        {
+            return browserName.Contains("Microsoft Edge") || browserName.Contains("EdgeChromium") || browserName.Contains("IE");
+        }
+    }
+}
